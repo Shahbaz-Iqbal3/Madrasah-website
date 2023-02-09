8a0452c6958543a629999d2c9c57db67c3ea753b
Shahbaz-Iqbal3 <shahbaziqbal233@gmail.com>
2023-02-09 22:45:04 +0500

    update ui

--- scripts/index.js
+++ scripts/index.js
@@ -1,9 +1,11 @@
 const slide = document.querySelector(".testimonial-slide")
 const slideBtn = document.querySelectorAll(".testimonial-btn")
+
+if (screen.width >= 1200 && screen.width <= 1400 ) {
 function slider(index, int) {
     slide.style.left = int
 }
-console.log(screen.width);
+console.log(screen.width );
 let index = 0;
 setInterval(() => {
     if (screen.width > 1150) {
@@ -15,3 +17,4 @@ setInterval(() => {
         }
     }
 }, 4000);
+}

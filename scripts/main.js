99192732ceee4181a876e182cdea534806a299d5
Shahbaz-Iqbal3 <shahbaziqbal233@gmail.com>
2023-03-15 13:01:46 +0500

    asd

--- scripts/main.js
+++ scripts/main.js
@@ -1,6 +1,6 @@
 // If absolute URL from the remote server is provided, configure the CORS
 // header on that server.
-var url = '/quran.pdf';
+var url = 'https://pwa-emacademy.netlify.app/quran.pdf';
 
 // Loaded via <script> tag, cre  ate shortcut to access PDF.js exports.
 var pdfjsLib = window['pdfjs-dist/build/pdf'];
